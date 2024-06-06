2dec9de09f20237d10fb026416145afead36f698
Favio Magallanes <magallanes.f@outlook.com>
2024-06-06 15:03:04 -0300

    feat: Add debounce functionality in useFilteredData

--- src/hooks/use-filtered-data.ts
+++ src/hooks/use-filtered-data.ts
@@ -1,9 +1,24 @@
-import { useState, useMemo } from "react";
+import { useState, useMemo, useEffect } from "react";
 
 type Stringable = string | number | boolean | null | undefined;
 
-export const useFilteredData = <T, K extends keyof T>(data: T[], key: K) => {
+export const useFilteredData = <T, K extends keyof T>(
+  data: T[],
+  key: K,
+  debounceTime: number = 300
+) => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState("");
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedSearchTerm(searchTerm);
+    }, debounceTime);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [searchTerm, debounceTime]);
 
   const filteredData = useMemo(() => {
     return data?.filter(item => {
@@ -11,9 +26,9 @@ export const useFilteredData = <T, K extends keyof T>(data: T[], key: K) => {
       return (value as Stringable)
         ?.toString()
         .toLowerCase()
-        .includes(searchTerm.toLowerCase());
+        .includes(debouncedSearchTerm.toLowerCase());
     });
-  }, [data, searchTerm, key]);
+  }, [data, debouncedSearchTerm, key]);
 
   return { searchTerm, setSearchTerm, filteredData };
 };
